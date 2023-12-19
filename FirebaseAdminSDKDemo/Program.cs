ea35ddbeb0cfc6db1d831e662427b6123dfa849c
tyr.wang <tyr.wang@hy-tech.com.tw>
2023-12-19 17:44:51 +0800

    Adds FirebaseAdmin

--- FirebaseAdminSDKDemo/Program.cs
+++ FirebaseAdminSDKDemo/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+
 Console.WriteLine("Hello, World!");
+
+var defaultApp = FirebaseApp.Create(new AppOptions()
+{
+	Credential = GoogleCredential.FromFile(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "secrets/fir-firebase-adminsdk.json")),
+});
+
+Console.WriteLine(defaultApp.Name);
